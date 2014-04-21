896ba0ae8f95e6827c0ceca2c83af0700754cf42
Angela Kao <angelakao@AngelatekiMacBook-Air.local>
2014-04-21 22:58:18 +0800

    Add dashboard css and apply

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Project Writer</title>
+		<!----Bootstrap core css-->
+		<link href="public/css/bootstrap.min.css" ref="stylesheet" >
+		<link href ="dashboard.css" ref="stylesheet">
+		<style type="text/css">
+		<style id = "holdejs-style" type = "text/css">
+	</head>
+	<body>
+		<div id = "top"></div>
+		<div id = "left_menu"></div>
+		<div id = "main">
+			<div id = "bar"></div>
+			<div id = "content"></div>
+		</div>
+	</body>
+</html>
